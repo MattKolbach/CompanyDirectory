17e57d7e5a9c0e514379548833b8be436cfe2488
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-26 15:49:57 -0600

    pushing for askBCS help

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,25 +10,26 @@ INSERT INTO role (role_id, title, salary, department_id)
 VALUES
   (1, 'Sales Lead', 100000, 1),
   (2, 'Salesperson', 80000, 1),
-  (3, 'Lead Engineer', 150000, 2),
-  (4, 'Software Engineer', 120000, 2),
+  (3, 'Lead Engineer', 150000, 4),
+  (4, 'Software Engineer', 120000, 4),
   (5, 'Account Manager', 160000, 3),
   (6, 'Accountant', 125000, 3),
-  (7, 'Leagal Team Lead', 250000, 4),
-  (8, 'Lawyer', 190000, 4);
+  (7, 'Leagal Team Lead', 250000, 2),
+  (8, 'Lawyer', 190000, 2);
   
 INSERT INTO employee (emp_id, first_name, last_name, empRole_id, manager_id)
 VALUES
   (1, 'James', 'Fraser', 1, NULL),
   (2, 'Jack', 'London', 1, NULL),
+  (9, 'Emil', 'Zola', 3, NULL),
+  (10, 'Sissy', 'Coalpits', 3, NULL),
+  (22, 'William', 'Morris', 6, NULL),
   (3, 'Robert', 'Bruce', 2, 1),
   (4, 'Peter', 'Greenaway', 2, 1),
   (5, 'Derek', 'Jarman', 2, 1),
   (6, 'Paolo', 'Pasolini', 2, 2),
   (7, 'Heathcote', 'Williams', 2, 2),
   (8, 'Sandy', 'Powell', 2, 2),
-  (9, 'Emil', 'Zola', 3, NULL),
-  (10, 'Sissy', 'Coalpits', 3, NULL),
   (11, 'Antoinette', 'Capet', 4, 9),
   (12, 'Samuel', 'Delany', 4, 9),
   (13, 'Tony', 'Duvert', 4, 9),
@@ -37,10 +38,9 @@ VALUES
   (16, 'Samuel', 'Johnson', 4, 10),
   (17, 'John', 'Dryden', 4, 10),
   (18, 'Alexander', 'Pope', 4, 10),
-  (19, 'Lionel', 'Johnson', 5, NULL),
-  (20, 'Aubrey', 'Beardsley', 5, NULL),
-  (21, 'Tulse', 'Luper', 5, NULL),
-  (22, 'William', 'Morris', 6, NULL),
+  (19, 'Lionel', 'Johnson', 5, 22),
+  (20, 'Aubrey', 'Beardsley', 5, 22),
+  (21, 'Tulse', 'Luper', 5, 22),
   (23, 'George', 'Shaw', 7, 5),
   (24, 'Arnold', 'Bennett', 8, 23),
   (25, 'Algernon', 'Blackwood', 8, 23);
--- server.js
+++ server.js
@@ -64,43 +64,58 @@ function init() {
     });
 }
 
-/////  switch functions to move later  /////
+//////////  switch functions to move later  //////////
 function viewAllDepartments() {
   db.query("SELECT * FROM department", (err, results) => {
     if (err) {
       console.log(err);
     }
+    console.log(
+      `+-------------------+
+|  ALL DEPARTMENTS  |
++-------------------+`
+    );
     console.table(results);
     init();
   });
 }
 
-//////////////////////////////
+//////////  view all roles  //////////
 function viewAllRoles() {
   db.query("SELECT * FROM role", (err, results) => {
     if (err) {
       console.log(err);
     }
+    console.log(
+      `+------------------------------------------------+
+|                  ALL ROLES                     |
++------------------------------------------------+`
+    );
     console.table(results);
     init();
   });
 }
 
-//////////////////////////////
+//////////  view all employees  //////////
 function viewAllEmployees() {
   db.query(
-    "SELECT employee.first_name, employee.last_name, manager.last_name AS manager, role.title, role.salary, department.deptName FROM employee LEFT JOIN role ON role.role_id = employee.empRole_id LEFT JOIN department ON department.dept_id = role.department_id LEFT JOIN employee manager ON employee.manager_id = manager.emp_id",
+    "SELECT employee.emp_id, employee.first_name, employee.last_name, manager.last_name AS manager, role.title, role.salary, department.deptName FROM employee LEFT JOIN role ON role.role_id = employee.empRole_id LEFT JOIN department ON department.dept_id = role.department_id LEFT JOIN employee manager ON employee.manager_id = manager.emp_id",
     (err, results) => {
       if (err) {
         console.log(err);
       }
+      console.log(
+        `+------------------------------------------------------------------------------+
+|                                ALL EMPLOYEES                                 |
++------------------------------------------------------------------------------+`
+      );
       console.table(results);
       init();
     }
   );
 }
 
-//////////////////////////////add a department
+//////////  add a department  //////////
 function addADepartment() {
   inquirer
     .prompt([
@@ -124,25 +139,163 @@ function addADepartment() {
         if (err) {
           console.log(err);
         }
-        console.log(result);
-        init();
+        //console.log(result);
+        viewAllDepartments();
       });
     });
 }
 
-//////////////////////////////
+//////////  add a role  //////////()()()()()()()()()()()()()()()()()()()()()()()()()
+// function addARole() {
+//   function viewAllDepartmentsTable() {
+//     db.query("SELECT * FROM department", (err, results) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       console.log(`
+// +-------------------+
+// |  ALL DEPARTMENTS  |
+// +-------------------+`
+//       );
+//       console.table(results);
+//     });
+//   }
+//   viewAllDepartmentsTable();
+//   inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         name: "title",
+//         message: "What ROLE would you like to add?",
+//         validate: (title) => {
+//           if (title) {
+//             return true;
+//           } else {
+//             console.log("Please enter the name of the new role.");
+//             return false;
+//           }
+//         },
+//       },
+//       {
+//         type: "input",
+//         name: "salary",
+//         message: "What is the base yearly salary for this position?",
+//         validate: (salary) => {
+//           if (salary) {
+//             return true;
+//           } else {
+//             console.log("Please enter a valid salary number.");
+//             return false;
+//           }
+//         },
+//       },
+//       {
+//         type: "input",
+//         name: "department_id",
+//         message: "What is is department ID that this position belongs to?",
+//         validate: (department_id) => {
+//           if (department_id) {
+//             return true;
+//           } else {
+//             console.log("Please enter a valid salary number.");
+//             return false;
+//           }
+//         },
+//       },
+//     ])
+//     .then((newRole) => {
+//       db.query(`INSERT INTO role SET ?`, newRole, (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         //console.log(result);
+//         viewAllRoles();
+//       });
+//     });
+// }
+///////////////////////()()()()()()()()()()()()()()()()()()()()()()()()()()()()
+
+//////////  create new role  //////////
+async function addARole() {
+  console.log(
+`+-----------------------------------------------------------+
+|                      CREATE NEW ROLE                      |
++-----------------------------------------------------------+`
+  );
+  const viewAllDepartments = await db
+    .promise()
+    .query(`SELECT dept_id AS value, deptName AS name FROM department`);
+
+  // const allRoles = await db
+  //   .promise()
+  //   .query(`SELECT role_id AS value, title AS name FROM role`);
+    
+  const createNewRole = await inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What ROLE would you like to add?",
+        validate: (title) => {
+          if (title) {
+            return true;
+          } else {
+            console.log("Please enter the name of the new role.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the base yearly salary for this position?",
+        validate: (salary) => {
+          if (salary) {
+            return true;
+          } else {
+            console.log("Please enter a valid salary number.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "dept_id",
+        message: "What department does this new role belong to?",
+        choices: viewAllDepartments[0],
+      },
+  ]);
+  const createNewRoleInSQL = await db
+    .promise()
+    .query(`INSERT INTO role ('title', 'salary', 'department_id') VALUES ('title = ?', 'salary = ?', 'dept_id = ?')`, [
+      createNewRole.title,
+      createNewRole.salary,
+      createNewRole.dept_id
+    ]);
+    
+  console.log("role created");
+  init();
+}
+
+//////////  update employee role  //////////
 async function updateEmpRole() {
+  console.log(
+`+-----------------------------------------------------------+
+|                  UPDATE EMPLOYEE ROLE                     |
++-----------------------------------------------------------+`
+  );
   const allEmployees = await db
     .promise()
     .query(`SELECT emp_id AS value, last_name AS name FROM employee`);
+
   const allRoles = await db
     .promise()
     .query(`SELECT role_id AS value, title AS name FROM role`);
+
   const empRoleUpdate = await inquirer.prompt([
     {
       type: "list",
       name: "emp_id",
-      message: "What employee is being getting a new role?",
+      message: "What employee is getting a new role?",
       choices: allEmployees[0],
     },
     {
@@ -152,8 +305,13 @@ async function updateEmpRole() {
       choices: allRoles[0],
     },
   ]);
-  const updateRoleInSQL = await db.promise().query(`UPDATE employee SET empRole_id = ? WHERE emp_id = ?`, [empRoleUpdate.role_id, empRoleUpdate.emp_id])
-  console.log("updated");
+  const updateRoleInSQL = await db
+    .promise()
+    .query(`UPDATE employee SET empRole_id = ? WHERE emp_id = ?`, [
+      empRoleUpdate.role_id,
+      empRoleUpdate.emp_id,
+    ]);
+  console.log("employee role updated");
   init();
 }
 
